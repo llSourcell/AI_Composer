2eaf4d7e5044ab9ffe16e42e174a853614e1625c
Yoav Zimmerman <yoavzimmerman@gmail.com>
2016-01-27 14:19:11 -0800

    fixups

--- rnn.py
+++ rnn.py
@@ -2,7 +2,7 @@ import os
  
 import numpy as np
 import tensorflow as tf    
-from tensorflow.models.rnn import rnn    
+from tensorflow.models.rnn import rnn
 from tensorflow.models.rnn.rnn_cell import BasicLSTMCell, LSTMCell    
 
 from midi_util import midi_to_sequence, sequence_to_midi
@@ -10,65 +10,118 @@ from model import Model
 
 DATA_DIR = 'data/JSBChorales'
 
+HIDDEN_SIZE = 100
+
 def prepare_data(dataset_name):
-  train_path = os.path.join(DATA_DIR, dataset_name)
-  train_files = [ os.path.join(train_path, f) for f in os.listdir(train_path)
+    train_path = os.path.join(DATA_DIR, dataset_name)
+    train_files = [ os.path.join(train_path, f) for f in os.listdir(train_path)
                   if os.path.isfile(os.path.join(train_path, f)) ] 
-  train_seq = [ midi_to_sequence(f) for f in train_files ]
+    train_seq = [ midi_to_sequence(f) for f in train_files ]
 
-  # number of examples 
-  N = len(train_seq)
-  # sequence length is the longest sequence
-  seq_length = max(len(t) for t in train_seq)
-  # dimension of input
-  max_note = max(max([max(c) for c in seq if len(c) > 0]) for seq in train_seq)
-  min_note = min(min([min(c) for c in seq if len(c) > 0]) for seq in train_seq)
+    # number of examples 
+    N = len(train_seq)
+    # sequence length is the longest sequence
+    seq_length = max(len(t) for t in train_seq)
+    # dimension of input
+    max_note = max(max([max(c) for c in seq if len(c) > 0]) for seq in train_seq)
+    min_note = min(min([min(c) for c in seq if len(c) > 0]) for seq in train_seq)
 
-  return (train_seq, seq_length, max_note)
+    return (train_seq, seq_length, max_note)
+
+def sequences_to_data(seqs, max_seq_length, D):
+    # (sequence length) X (num sequences) X (dimension)
+    data = np.zeros([max_seq_length, len(seqs), D], dtype=np.float32)
+    for idx, seq in enumerate(seqs):
+      for c_idx, chord in enumerate(seq):
+          data[c_idx, idx, chord] = 1
+
+    # roll back the time steps axis to get the target of each example
+    targets = np.roll(data, -1, axis=0)
+    # set final time step targets to 0
+    targets[-1, :, :] = 0 
+
+    return (data, targets)
+
+def run_epoch(session, model, seq_data, seq_targets):
+    _, loss_value = session.run(
+        [model.train_step, model.loss],
+        feed_dict={
+            model.seq_input: seq_data,
+            model.seq_targets: seq_targets
+        })
+    return loss_value
 
 if __name__ == '__main__':
-  np.random.seed(1)      
+    np.random.seed(1)      
 
-  train_seqs, train_len, train_max = prepare_data('train')
-  valid_seqs, valid_len, valid_max = prepare_data('valid')
-  test_seqs, test_len, test_max = prepare_data('valid')
+    train_seqs, train_len, train_max = prepare_data('train')
+    valid_seqs, valid_len, valid_max = prepare_data('valid')
+    test_seqs, test_len, test_max = prepare_data('valid')
 
-  D = max(train_max, valid_max, test_max)+1
+    D = max(train_max, valid_max, test_max)+1
 
-  print 'Dimension: {}'.format(D)
+    print 'Dimension: {}'.format(D)
 
-  # TRAINING
-  # (sequence length) X (num sequences) X (dimension)
-  train_data = np.zeros([train_len, len(train_seqs), D], dtype=np.float32)
-  for idx, seq in enumerate(train_seqs):
-      for c_idx, chord in enumerate(seq):
-          train_data[c_idx, idx, chord] = 1
+    train_data, train_targets = sequences_to_data(train_seqs, train_len, D)
+    test_data, test_targets = sequences_to_data(test_seqs, test_len, D)
+    valid_data, valid_targets = sequences_to_data(valid_seqs, valid_len, D)
 
-  # roll back the time steps axis to get the target of each example
-  train_targets = np.roll(train_data, -1, axis=0)
-  # set final time step targets to 0
-  train_targets[-1, :, :] = 0 
+    with tf.Graph().as_default(), tf.Session() as session:
 
-  model = Model(len(train_seqs), D, train_len, hidden_size=100, initialization=0.1)
-  model.train(train_data, train_targets)
+        initializer = tf.random_uniform_initializer(-0.1, 0.1)
 
-  # TESTING
-  test_data = np.zeros([test_len, len(test_seqs), D], dtype=np.float32)
-  for idx, seq in enumerate(test_seqs):
-      for c_idx, chord in enumerate(seq):
-          test_data[c_idx, idx, chord] = 1
+        with tf.variable_scope("model", reuse=None):
+            train_model = Model(len(train_seqs), D, train_len, hidden_size=HIDDEN_SIZE)
+        with tf.variable_scope("model", reuse=True):
+            valid_model = Model(len(valid_seqs), D, valid_len, hidden_size=HIDDEN_SIZE)
+            test_model = Model(len(test_seqs), D, test_len, hidden_size=HIDDEN_SIZE)
 
-  # roll back the time steps axis to get the target of each example
-  test_targets = np.roll(test_data, -1, axis=0)
-  # set final time step targets to 0
-  test_targets[-1, :, :] = 0 
+        tf.initialize_all_variables().run()
 
-  model = Model(len(test_seqs), D, test_len, hidden_size=100, initialization=0.1)
-  print model.test(test_data, test_targets)
+        # training
+        num_epochs = 25
+        learning_rate = 0.1
+        train_model.assign_lr(session, learning_rate)
+        for i in range(num_epochs):
+            loss = run_epoch(session, train_model, train_data, train_targets)
+            if i % 5 == 0:
+                print 'Epoch: {}, Loss: {}'.format(i, loss)
+        
+        # testing
+        loss = run_epoch(session, test_model, test_data, test_targets)
+        print 'Testing loss: {}'.format(loss)
 
-  # sampling
-  # model = Model(N, D, seq_length, hidden_size=100, 
-  #               initialization=0.1, sampling_mode=True)
-  # seq = model.sample(np.zeros(D), temperature=0.2)
-  # chords = [np.nonzero(c)[0].tolist() for c in seq]
-  # sequence_to_midi(chords, "gen1.midi")
+        # sampling
+        def sample_notes_from_probs(probs):
+            reshaped = np.reshape(probs, [D])
+            top_idxs = np.argpartition(reshaped, 4)[-4:]
+            chord = np.zeros([D], dtype=np.float32)
+            chord[top_idxs] = 1.0
+            return chord
+
+        seq_length = 100
+        # start with the first chord
+        with tf.variable_scope("model", reuse=True):
+            sampling_model = Model(1, D, 1, hidden_size=HIDDEN_SIZE)
+
+        chord = train_data[0, 0, :]
+        seq = [chord]
+        state = sampling_model.cell.zero_state(sampling_model.N, tf.float32) \
+                              .eval(session=session)
+
+        for i in range(seq_length):
+            seq_input = np.reshape(chord, [1, 1, D])
+            feed = {
+                sampling_model.seq_input: seq_input,
+                sampling_model.initial_state: state
+            }
+            [probs, state] = session.run(
+                [sampling_model.probs, sampling_model.final_state],
+                feed_dict=feed)
+            chord = sample_notes_from_probs(probs)
+            print chord
+            print state
+            seq.append(chord)
+         
+        chords = [np.nonzero(c)[0].tolist() for c in seq]
+        sequence_to_midi(chords, "gen1.midi")
