d311f4f57b6963d282672b2dec206f1095f9e900
Tales <taleslimaf@gmail.com>
2017-02-24 17:58:39 -0300

    Fixing: 'This module is deprecated'

--- model.py
+++ model.py
@@ -50,7 +50,7 @@ class Model(object):
             else:
                 raise Exception("Invalid cell type: {}".format(cell_type))
 
-            cell = cell_class(hidden_size, num_units=input_size)
+            cell = cell_class(hidden_size)
             if training:
                 return tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=dropout_prob)
             else:
